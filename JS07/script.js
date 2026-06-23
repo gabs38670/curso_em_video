f373a7c72473319daf14064207419326b8aa3cc3
gabs38670 <gabriel.frodrigues15@senacsp.edu.br>
2026-06-23 15:19:24 -0300

    aprendendo operadorers parte(1)

---
+++ JS07/script.js
@@ -0,0 +1,21 @@
+var a = 5 + 3;
+var b = a % 5;
+var c = 5 * b ** 2;
+var d = 10 - a / 2
+var e = 6 * 2 / d
+var f = b % e + 4 / e
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
+console.log(f)
+
+
+var n = 3
+n = n + 4 // n += 4
+n= n - 5 // n -= 5
+n = n * 4 // n *= 4
+n = n / 2 // n /= 2
+n = n ** 2 // n **= 2
+n = n % 5 // n %= 5
