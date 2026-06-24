4bdb63246cc312161921713e1681a27c7596dc93
gabs38670 <gabriel.frodrigues15@senacsp.edu.br>
2026-06-24 14:59:12 -0300

    Aula de concições parte (1) com exercícios

---
+++ JS11/ex008.js
@@ -0,0 +1,11 @@
+/*
+    Condições serve para determinar uma ação é executado ou não
+*/
+
+var vel = 55
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) { //Condição simples!
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança!`)
