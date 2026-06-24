9b77a5083bf9e9ce2a8beb51e22ab03da2e86cc2
gabs38670 <gabriel.frodrigues15@senacsp.edu.br>
2026-06-24 18:28:43 -0300

    Estrutura de repertição parte(1) while/do while

---
+++ JS13/script.js
@@ -0,0 +1,13 @@
+var c = 1;
+//Estrutura de repetição simple com While que significa enquanto
+//No while primeiro faz o teste e depois executa
+// while(c <= 6){
+//     console.log(`Passo ${c}`)
+//     c++
+// }
+
+//DO Wilhe primeiro executa o bloco e depois faz o teste
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
